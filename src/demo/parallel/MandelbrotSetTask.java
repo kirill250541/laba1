65cdfd29e71aefdf8b5c523c9a0d08db59e142de
Kirill <ksemenenya@bk.ru>
2026-04-27 01:53:02 +0300

    change formula

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -262,7 +262,7 @@ class MandelbrotSetTask extends Task<Long> {
     }
 
     /**
-     * Calculates number of iterations a complex quadratic polynomials
+     * Calculates number of iterations a complex cubic polynomial
      * stays within a disk of some finite radius for a given complex number.
      * 
      * This number is used to choose a color for this pixel for precalculated 
@@ -275,7 +275,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).plus(comp);
+            c = c.times(c).times(c).plus(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
